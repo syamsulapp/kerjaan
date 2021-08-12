5d123d1c51ad1150a8c25577dec8dde1242fde96
syamsulapp <samsul.marif100@gmail.com>
2021-08-13 07:51:25 +0800

    tampilan login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,6 +33,12 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        // costumisasi nama validasinya
+        // $validasi = [
+        //     'email' => [
+        //         'required' => 'Penulisan Email Tidak Sesuai'
+        //     ]
+        // ];
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
---
+++ resources/views/layouts/tema/auth/login.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.tema.app')
+
+@section('title','Halaman Login')
+
+@section('konten')
+
+<body class="form">
+    <div class="form-container">
+        <div class="form-form">
+            <div class="form-form-wrap">
+                <div class="form-container">
+                    <div class="form-content">
+
+                        <h1 class="">{{ ('Login Di') }} <a href="index.html"><span class="brand-name">{{ ('SIOLA') }}</span></a></h1>
+                        <p class="signup-link">{{ ('Belum Punya Akun?') }} <a href="auth_register.html">{{ ('Buat Akun') }}</a></p>
+
+                        @if($errors->any())
+                            <div class="alert alert-danger">    
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form class="text-left" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form">
+
+                                <div id="username-field" class="field-wrapper input">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-user">
+                                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                        <circle cx="12" cy="7" r="4"></circle>
+                                    </svg>
+                                    <input id="email" name="email" type="text" class="form-control"
+                                        placeholder="email" value="{{ old('email') }}">
+                                </div>
+
+                                <div id="password-field" class="field-wrapper input mb-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-lock">
+                                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                                    </svg>
+                                    <input id="password" name="password" type="password" class="form-control"
+                                        placeholder="Password" value="{{ old('email') }}">
+                                </div>
+                                <div class="d-sm-flex justify-content-between">
+                                    <div class="field-wrapper toggle-pass">
+                                        <p class="d-inline-block">Show Password</p>
+                                        <label class="switch s-primary">
+                                            <input type="checkbox" id="toggle-password" class="d-none">
+                                            <span class="slider round"></span>
+                                        </label>
+                                    </div>
+                                    <div class="field-wrapper">
+                                        <!-- <button type="submit" class="btn btn-primary" value="">Log In</button> -->
+                                        <button type="submit" class="btn btn-primary"> {{ ('Masuk') }}</button>
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        </form>
+                        <p class="terms-conditions">© {{ now()->year }} {{ ('SIOLA KEMENDAGRI') }} {{ ('Kota Kendari') }}</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form-image">
+            <div class="l-image">
+            </div>
+        </div>
+    </div>
+
+@endsection
