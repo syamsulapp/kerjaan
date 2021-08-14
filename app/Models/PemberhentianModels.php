e3a0b4428c8aca8965e87376dd1e766c2b5a17bb
syamsulapp <samsul.marif100@gmail.com>
2021-08-14 22:45:12 +0800

    perbaikan logic form

--- app/Models/PemberhentianModels.php
+++ app/Models/PemberhentianModels.php
@@ -11,5 +11,5 @@ class PemberhentianModels extends Model
 
     protected $table = 'table_pemberhentian';
 
-    protected $fillable = ['noreg', 'jk', 'objekkdh', 'kabupaten', 'penyebabkdh', 'nama', 'provinsi', 'upload1', 'upload2', 'upload3', 'upload4', 'upload5', 'upload6'];
+    protected $fillable = ['noreg', 'jk', 'kategori_permohonan', 'objekkdh', 'kabupaten', 'penyebabkdh', 'nama', 'provinsi', 'upload1', 'upload2', 'upload3', 'upload4', 'upload5', 'upload6'];
 }
