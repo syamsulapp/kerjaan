3657d02e65866adc65b44ef295b15e5132693fc8
syamsulapp <samsul.marif100@gmail.com>
2021-08-16 14:49:32 +0800

    fix fronts lagi

--- resources/views/layouts/tema/depan/depan.blade.php
+++ resources/views/layouts/tema/depan/depan.blade.php
@@ -92,7 +92,7 @@
                             <li><a href="services.html">Survei Index Kepuasan Masyarakat 2020</a></li>
                         </ul>
                     </li>
-                    <li><a href="{{ route('login'}}l"><svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+                    <li><a href="{{ route('login') }} "><svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
                                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                 <circle cx="12" cy="7" r="4"></circle>
                             </svg> Login</a></li>
