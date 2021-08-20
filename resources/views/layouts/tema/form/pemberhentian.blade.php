f6ef1dabf6d812af33106378bc47fa83fc094c45
arif <me@system112.org>
2021-08-21 02:21:29 +0800

    edit

--- resources/views/layouts/tema/form/pemberhentian.blade.php
+++ resources/views/layouts/tema/form/pemberhentian.blade.php
@@ -134,7 +134,7 @@
                                                                 </label>
                                                                 <div class="col-xl-5 col-lg-9 col-sm-10">
                                                                     <select class="form-control  basic" name="objekkdh">
-                                                                        @foreach($data['objekkdh'] as $objek => $kdh)
+                                                                        @foreach($data["objekkdh"] as $objek => $kdh)
                                                                             <option value="{{ $kdh->objekkdh }}" selected >{{ $kdh->objekkdh }}</option>
                                                                         @endforeach
                                                                     </select>
@@ -148,7 +148,7 @@
                                                                 </label>
                                                                 <div class="col-xl-5 col-lg-9 col-sm-10">
                                                                     <select class="form-control  basic" name="kabupaten">
-                                                                        @foreach($data['kabupaten'] as $data => $kabupaten)
+                                                                        @foreach($data["kabupaten"] as $data => $kabupaten)
                                                                             <option value="{{ $kabupaten->kabupaten }}" selected >{{ $kabupaten->kabupaten }}</option>
                                                                         @endforeach
                                                                     </select>
