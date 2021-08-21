14d7e0642b760e02860fb87dcc4170a016319289
arif <me@system112.org>
2021-08-21 13:32:09 +0800

    usululan view

--- resources/views/layouts/tema/usulan/usulanview.blade.php
+++ resources/views/layouts/tema/usulan/usulanview.blade.php
@@ -16,7 +16,7 @@
 
         <div class="content-container">
 
-            <div class="layout-top-spacing">
+            <div class="col-left layout-top-spacing">
                 <div class="col-left-content">
 
                     <div class="header-container">
@@ -95,81 +95,6 @@
                         </div>
 
                     </div>
-
-
-                    <div class="admin-data-content layout-top-spacing">
-
-                                <div class="row layout-top-spacing" id="cancel-row">
-
-                                    <div class="col-xl-14 col-lg-15 col-sm-14  layout-spacing">
-                                        <div class="widget-content widget-content-area br-6">
-                                            <table id="zero-config" class="table dt-table-hover" style="width:100%">
-                                                <thead>
-                                                    <tr>
-                                                        <th>No</th>
-                                                        <th>Nama</th>
-                                                        <th>Tgl Registrasi</th>
-                                                        <th>Nomor Registrasi</th>
-                                                        <th>Layanan</th>
-                                                        <th>Informasi Permohonan</th>
-                                                        <th>Status Permohonan</th>
-                                                        <th class="no-content">Actions</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th>No</th>
-                                                        <th>Nama</th>
-                                                        <th>Tgl Registrasi</th>
-                                                        <th>Nomor Registrasi</th>
-                                                        <th>Layanan</th>
-                                                        <th>Informasi Permohonan</th>
-                                                        <th>Status Permohonan</th>
-                                                        <td>
-                                                            <button class="btn btn-danger mb-2"><svg viewBox="0 0 24 24"
-                                                                    width="24" height="24" stroke="currentColor"
-                                                                    stroke-width="2" fill="none" stroke-linecap="round"
-                                                                    stroke-linejoin="round" class="css-i6dzq1">
-                                                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                                                    <path
-                                                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                                    </path>
-                                                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                                                </svg> delete</button>
-                                                            <a href="formuser.html" class="btn btn-info mb-2"><svg
-                                                                    viewBox="0 0 24 24" width="24" height="24"
-                                                                    stroke="currentColor" stroke-width="2" fill="none"
-                                                                    stroke-linecap="round" stroke-linejoin="round"
-                                                                    class="css-i6dzq1">
-                                                                    <line x1="8" y1="6" x2="21" y2="6"></line>
-                                                                    <line x1="8" y1="12" x2="21" y2="12"></line>
-                                                                    <line x1="8" y1="18" x2="21" y2="18"></line>
-                                                                    <line x1="3" y1="6" x2="3.01" y2="6"></line>
-                                                                    <line x1="3" y1="12" x2="3.01" y2="12"></line>
-                                                                    <line x1="3" y1="18" x2="3.01" y2="18"></line>
-                                                                </svg> Detail</a>
-                                                            <a href="formuser.html" class="btn btn-warning mb-2"><svg
-                                                                    viewBox="0 0 24 24" width="24" height="24"
-                                                                    stroke="currentColor" stroke-width="2" fill="none"
-                                                                    stroke-linecap="round" stroke-linejoin="round"
-                                                                    class="css-i6dzq1">
-                                                                    <path d="M12 20h9"></path>
-                                                                    <path
-                                                                        d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z">
-                                                                    </path>
-                                                                </svg> Edit</a>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
                     <div class="footer-wrapper col-xl-12">
                         <div class="footer-section f-section-1">
                             <p class="">© {{ now()->year }} <a target="_blank" href="https://designreset.com">SIOLA_Prov KDH</a>
