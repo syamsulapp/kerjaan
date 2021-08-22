0ed254e07a57a0f80c8ed90503d24437e4d2e13e
syamsulapp <samsul.marif100@gmail.com>
2021-08-22 17:21:49 +0800

    chat whatsapp

--- resources/views/layouts/tema/app.blade.php
+++ resources/views/layouts/tema/app.blade.php
@@ -49,10 +49,9 @@
     <!-- <link href="css/skins/square/grey.css" rel="stylesheet"> -->
 
     <!-- Style of the plugin -->
-    <!-- whatsapp -->
+    <!-- whatsapp css -->
         <link rel="stylesheet" href="{{ asset('plugin/whatsapp-chat-support.css') }}">
 </head>
-
 
 
 @yield('konten')
@@ -133,6 +132,7 @@
 <!-- Plugin JS file -->
 <script src="{{ asset('plugin/components/moment/moment.min.js') }}"></script>
 <script src="{{ asset('plugin/components/moment/moment-timezone-with-data.min.js') }}"></script> <!-- spanish language (es) -->
+<script src="{{ asset('plugin/components/moment/moment-timezone-with-data-10-year-range.min.js') }}"></script>
 <script src="{{ asset('plugin/whatsapp-chat-support.js') }}"></script>
 
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
@@ -146,6 +146,77 @@
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="{{ asset('js/wizard_func_multiple_branch.js') }}"></script> -->
 <!-- tutup -->
+
+<script>
+		$('#example').whatsappChatSupport();
+		$('#example_1').whatsappChatSupport();
+
+		$('#example_2').whatsappChatSupport();
+
+		$('#example_3').whatsappChatSupport({
+			defaultMsg : '',
+		});
+
+		$('#example_4').whatsappChatSupport({
+			defaultMsg : '',
+		});
+
+		$('#example_5').whatsappChatSupport();
+
+		$('#example_6').whatsappChatSupport();
+
+		$('#example_7').whatsappChatSupport();
+
+		$('#example_8').whatsappChatSupport();
+
+		$('#example_9').whatsappChatSupport();
+
+		$('#example_10').whatsappChatSupport();
+
+		$('#example_11').whatsappChatSupport();
+		
+		$('#example_12').whatsappChatSupport();
+
+		$('#example_13').whatsappChatSupport();
+	</script>
+    <script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
 </body>
+
 
 </html>
--- resources/views/layouts/tema/dashboard/dashboard.blade.php
+++ resources/views/layouts/tema/dashboard/dashboard.blade.php
@@ -75,45 +75,73 @@
                                         </div>
                                     </div>
                                     @else
-                                    <!-- whatsapp -->
-                                    <div class="whatsapp_chat_support wcs_fixed_right" id="example_3">
-                                            <div class="wcs_button_label">
-                                                    Questions? Let's Chat
-                                                </div>  
-                                            <div class="wcs_button wcs_button_circle">
-                                                <span class="fa fa-whatsapp"></span>
-                                            </div>  
-                                        
-                                            <div class="wcs_popup">
-                                                <div class="wcs_popup_close">
-                                                    <span class="fa fa-close"></span>
-                                                </div>
-                                                <div class="wcs_popup_header">
-                                                    <span class="fa fa-whatsapp"></span>
-                                                    <strong>Customer Support</strong>
-                                                    
-                                                    <div class="wcs_popup_header_description">Need Help? Chat with us on Whatsapp</div>
-                                                </div>  
-                                                <div 
-                                                    class="wcs_popup_input" 
-                                                    data-number="528123861273"
-                                                    data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
-                                                >
-                                                    <input type="text" placeholder="Ask anything!" />
-                                                    <i class="fa fa-play"></i>
-                                                </div>
-                                                <div class="wcs_popup_avatar">
-                                                    <img src="img/person_5.jpg" alt="">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <!-- // end whatsapp -->
                                     <div class="alert alert-success" role="alert">
                                         <h4 class="alert-heading">Anda Sudah Mengirim Permohonan</h4>
                                         <p>silahkan cek status data anda.</p>
                                         <hr>
                                         <p class="mb-0">{{ __('@') }} {{ now()->year }} {{ _('SIOLA_Prov KDH')}} {{ __('reserved') }}.</p>
                                     </div>
+                                    <!-- whatsapp -->
+                                    <div class="whatsapp_chat_support wcs_fixed_right" id="example">
+                                                <div class="wcs_button_label">
+                                                    Questions? Let's Chat
+                                                </div>  
+                                                <div class="wcs_button wcs_button_circle">
+                                                    <span class="fa fa-whatsapp"></span>
+                                                </div>  
+                                            
+                                                <div class="wcs_popup">
+                                                    <div class="wcs_popup_close">
+                                                        <span class="fa fa-close"></span>
+                                                    </div>
+                                                    <div class="wcs_popup_header">
+                                                        <strong>Need Help? Chat with us</strong>
+                                                        <br>
+                                                        <div class="wcs_popup_header_description">Click one of our representatives below</div>
+                                                    </div>  
+                                                    <div class="wcs_popup_person_container">
+                                                        <div 
+                                                            class="wcs_popup_person" 
+                                                            data-number="528123861273"
+                                                            data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
+                                                        >
+                                                            <div class="wcs_popup_person_img"><img src="{{ asset('img/person_5.jpg') }}" alt=""></div>
+                                                            <div class="wcs_popup_person_content">
+                                                                <div class="wcs_popup_person_name">Mia Smith</div>
+                                                                <div class="wcs_popup_person_description">Sales Support</div>
+                                                                <div class="wcs_popup_person_status">I'm Online</div>
+                                                            </div>  
+                                                        </div>
+                                            
+                                                        <div 
+                                                            class="wcs_popup_person" 
+                                                            data-number="528123861273"
+                                                            data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
+                                                        >
+                                                            <div class="wcs_popup_person_img"><img src="{{ asset('img/person_6.jpg') }}" alt=""></div>
+                                                            <div class="wcs_popup_person_content">
+                                                                <div class="wcs_popup_person_name">James Brown</div>
+                                                                <div class="wcs_popup_person_description">Customer Support</div>
+                                                                <div class="wcs_popup_person_status">I'm Online</div>
+                                                            </div>
+                                                        </div>
+                                            
+                                                        <div 
+                                                            class="wcs_popup_person" 
+                                                            data-number="528123861273"
+                                                            data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
+                                                        >
+                                                            <div class="wcs_popup_person_img"><img src="{{ asset('img/person_7.jpg') }}" alt=""></div>
+                                                            <div class="wcs_popup_person_content">
+                                                                <div class="wcs_popup_person_name">Robert Miller</div>
+                                                                <div class="wcs_popup_person_description">Techincal Support</div>
+                                                                <div class="wcs_popup_person_status">I'm Online</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>  
+                                    <!-- // end whatsapp -->
                                     @endif
                                 </div>
                             </div>
