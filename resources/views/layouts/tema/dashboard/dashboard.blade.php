3fb68fab2fba80c51c728a8634ab709786d149b8
arif <me@system112.org>
2021-08-22 15:32:58 +0800

    tcpdf

--- resources/views/layouts/tema/dashboard/dashboard.blade.php
+++ resources/views/layouts/tema/dashboard/dashboard.blade.php
@@ -75,65 +75,6 @@
                                         </div>
                                     </div>
                                     @else
-                                    <div class="whatsapp_chat_support wcs_fixed_right" id="example">
-    <div class="wcs_button_label">
-        Questions? Let's Chat
-    </div>  
-    <div class="wcs_button wcs_button_circle">
-        <span class="fa fa-whatsapp"></span>
-    </div>  
- 
-    <div class="wcs_popup">
-        <div class="wcs_popup_close">
-            <span class="fa fa-close"></span>
-        </div>
-        <div class="wcs_popup_header">
-            <strong>Need Help? Chat with us</strong>
-            <br>
-            <div class="wcs_popup_header_description">Click one of our representatives below</div>
-        </div>  
-        <div class="wcs_popup_person_container">
-            <div 
-                class="wcs_popup_person" 
-                data-number="528123861273"
-                data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
-            >
-                <div class="wcs_popup_person_img"><img src="img/person_5.jpg" alt=""></div>
-                <div class="wcs_popup_person_content">
-                    <div class="wcs_popup_person_name">Mia Smith</div>
-                    <div class="wcs_popup_person_description">Sales Support</div>
-                    <div class="wcs_popup_person_status">I'm Online</div>
-                </div>  
-            </div>
- 
-            <div 
-                class="wcs_popup_person" 
-                data-number="528123861273"
-                data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
-            >
-                <div class="wcs_popup_person_img"><img src="img/person_6.jpg" alt=""></div>
-                <div class="wcs_popup_person_content">
-                    <div class="wcs_popup_person_name">James Brown</div>
-                    <div class="wcs_popup_person_description">Customer Support</div>
-                    <div class="wcs_popup_person_status">I'm Online</div>
-                </div>
-            </div>
- 
-            <div 
-                class="wcs_popup_person" 
-                data-number="528123861273"
-                data-availability='{ "monday":"08:30-18:30", "tuesday":"08:30-18:30", "wednesday":"08:30-18:30", "thursday":"08:30-18:30", "friday":"08:30-18:30" }'
-            >
-                <div class="wcs_popup_person_img"><img src="img/person_7.jpg" alt=""></div>
-                <div class="wcs_popup_person_content">
-                    <div class="wcs_popup_person_name">Robert Miller</div>
-                    <div class="wcs_popup_person_description">Techincal Support</div>
-                    <div class="wcs_popup_person_status">I'm Online</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>  
                                     <div class="alert alert-success" role="alert">
                                         <h4 class="alert-heading">Anda Sudah Mengirim Permohonan</h4>
                                         <p>silahkan cek status data anda.</p>
